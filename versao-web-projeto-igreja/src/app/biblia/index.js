ea7e4da52a208676b40baea1a8e876f79cfa0d65
jessicaprestelo1201 <jessica.p.jesus@aluno.senai.br>
2025-05-27 15:58:46 -0300

    feat: correção da page.js

--- versao-web-projeto-igreja/src/app/biblia/index.js
+++
@@ -1,9 +0,0 @@
-import { View, Text } from 'react-native';
-
-export default function BibliaIndex() {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Seção da Bíblia</Text>
-    </View>
-  );
-}
---
+++ versao-web-projeto-igreja/src/app/biblia/page.js
@@ -0,0 +1,7 @@
+export default function BibliaIndex() {
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+      <h1>Seção da Bíblia</h1>
+    </div>
+  );
+}
